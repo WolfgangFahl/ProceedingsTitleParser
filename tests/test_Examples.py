b0a98fa7b0b2b896aabeddb1dbd57bd086c0098e
Wolfgang Fahl <wf@bitplan.com>
2020-08-29 16:47:15 +0200

    fixes examples full monty and count of examples in testExamples

--- tests/test_Examples.py
+++ tests/test_Examples.py
@@ -23,7 +23,7 @@ class TestExamples(unittest.TestCase):
         '''
         examples=Lookup.getExamples()
         print (examples)
-        self.assertEqual(14,len(examples.keys()))
+        self.assertEqual(15,len(examples.keys()))
         pass
 
 if __name__ == "__main__":
