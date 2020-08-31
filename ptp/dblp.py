99351c1743f1cff5b9ced2fa44614621370a2bc5
Wolfgang Fahl <wf@bitplan.com>
2020-08-31 14:01:47 +0200

    tries fixing issue of travis build 204 by ignoring publtype which is only used ttwice in over 40.000 entries

--- ptp/dblp.py
+++ ptp/dblp.py
@@ -38,36 +38,41 @@ class Dblp(object):
                 rawevent['acronym']="%s %s" % (rawevent['booktitle'],rawevent['year'])
             if '@mdate' in rawevent:
                 rawevent['mdate']=rawevent.pop('@mdate')
-            if '@publtype' in rawevent:
-                rawevent['publtype']=rawevent.pop('@publtype')    
-            event=Event()
-            for checkKey in ['booktitle','ee','editor','isbn','lookupAcronym','publisher','series','title','volume','url']:
-                if not checkKey in rawevent:
-                    rawevent[checkKey]=None
-                else:
-                    value=rawevent[checkKey]
-                    if type(value) is list:
-                        value=value[0]
-                        if warnings<maxWarnings:
-                            print("warning %s has %d values" %(checkKey,len(value)))
-                            warnings+=1
-                        if checkKey=='editor':
-                            if '@orcid' in value:
-                                rawevent['editorOrcid']=value['@orcid']
-                        rawevent[checkKey]=value
-                    if type(value) is dict:    
-            #  'ee': {'@type': 'oa', '#text': 'http://ceur-ws.org/Vol-1974'} 
-            # 'title': {'sup': 'th', '#text': 'Usability ...           
-                        value=value['#text']   
-                        rawevent[checkKey]=value 
-            self.em.setNone(rawevent, ['editorOrcid'])            
-            event.fromDict(rawevent)
-            event.source=self.em.name
-            # make sure the values are set and there are not multiple ones
-            # if the value is a dict extract the #text attribute        
-            if 'url' in rawevent:
-                event.url='https://dblp.org/%s' % rawevent['url']
-            self.em.add(event)    
+            else:
+                rawevent['mdate']=None
+            # publtype informal/withdrawn only used 2 x in over 40.0000 entries ...
+            if not '@publtype' in rawevent:
+            #    rawevent['publtype']=rawevent.pop('@publtype')    
+            #else:
+            #    rawevent['publtype']=None    
+                event=Event()
+                for checkKey in ['booktitle','ee','editor','isbn','lookupAcronym','publisher','series','title','volume','url']:
+                    if not checkKey in rawevent:
+                        rawevent[checkKey]=None
+                    else:
+                        value=rawevent[checkKey]
+                        if type(value) is list:
+                            value=value[0]
+                            if warnings<maxWarnings:
+                                print("warning %s has %d values" %(checkKey,len(value)))
+                                warnings+=1
+                            if checkKey=='editor':
+                                if '@orcid' in value:
+                                    rawevent['editorOrcid']=value['@orcid']
+                            rawevent[checkKey]=value
+                        if type(value) is dict:    
+                #  'ee': {'@type': 'oa', '#text': 'http://ceur-ws.org/Vol-1974'} 
+                # 'title': {'sup': 'th', '#text': 'Usability ...           
+                            value=value['#text']   
+                            rawevent[checkKey]=value 
+                self.em.setNone(rawevent, ['editorOrcid'])            
+                event.fromDict(rawevent)
+                event.source=self.em.name
+                # make sure the values are set and there are not multiple ones
+                # if the value is a dict extract the #text attribute        
+                if 'url' in rawevent:
+                    event.url='https://dblp.org/%s' % rawevent['url']
+                self.em.add(event)    
         self.em.store()        
                 
     def initEventManager(self):
