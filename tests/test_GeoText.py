4233faf5edeb461e352fb628f1da2ab09b274d88
Wolfgang Fahl <wf@bitplan.com>
2020-09-21 09:04:38 +0200

    checks geograpy hits to be over 60%

--- tests/test_GeoText.py
+++ tests/test_GeoText.py
@@ -5,9 +5,11 @@ Created on 2020-09-08
 '''
 import unittest
 from ptp.lookup import Lookup
+from lodstorage.sql import SQLDB
 from geotext import GeoText
 
 import geograpy
+import os
 
 class TestGeoText(unittest.TestCase):
     '''
@@ -22,34 +24,70 @@ class TestGeoText(unittest.TestCase):
         pass
 
 
-    def testGeoText(self):
+    def testGeoTextAndGrapy(self):
         '''
-        test the GeoText library
+        test the GeoText and geograpy3 library
         '''
-        
+        debug=True
+        limit=100
         sqlQuery="""select count(*) as count,
 locality from Event_wikicfp
 where locality is not null
 group by locality
 order by 1 desc
-LIMIT 100
-"""
-        lookup=Lookup.ensureAllIsAvailable("testGeoText")
-        sqlDB=lookup.getSQLDB()
+LIMIT %d
+""" % limit
+        dbFile=Lookup.getDBFile()
+        if os.path.isfile(dbFile):
+            sqlDB=SQLDB(dbFile)
+        else:
+            lookup=Lookup.ensureAllIsAvailable("testGeoText")
+            sqlDB=lookup.getSQLDB()
         if sqlDB is not None:
             print("testGeoText from database %s " % sqlDB.dbname)
+            totalResult=sqlDB.query("""select count(*) as count
+  from event_wikicfp
+  where locality is not null""")
+            total=totalResult[0]['count']
             listOfDicts=sqlDB.query(sqlQuery)
+            index=0
+            rsum=0
+            found=0
             for record in listOfDicts:
                 locality=record['locality']
-                print(locality)
+                count=record['count']
+                index+=1
+                rsum+=count
+                print("%5d: %5d/%5d %5.1f%%=%s" %(index,count,rsum,rsum/total*100,locality))
                 geo=GeoText(locality)
-                print(" %s" % geo.countries)
-                print(" %s" % geo.cities)
-                gp=geograpy.get_place_context(text=locality)
-                print(" %s" % gp.countries)
-                print(" %s" % gp.regions)
-                print(" %s" % gp.cities)
+                if debug:
+                    print("  %s" % geo.countries)
+                    print("  %s" % geo.cities)    
+                city=geograpy.locate(locality)
+                if city is not None:
+                    found+=1
+                if debug:
+                    print("  %s" % city)
+            if self.debug:        
+                print ("found %d/%d = %5.1f%%" % (found,limit,found/limit*100))
+            self.assertTrue(found/limit>0.6)
         pass
+    
+    def testExamples(self):
+        examples=[
+            'Singapore',
+            'Beijing, China',
+            'Paris, France',
+            'Barcelona, Spain',
+            'Rome, Italy',
+            'Hong Kong',
+            'Bangkok, Thailand',
+            'Vienna, Austria',
+            'Athens, Greece',
+            'Shanghai, China']
+        for example in examples:
+            geo=GeoText(example)
+            print (geo.countries)
 
 
 if __name__ == "__main__":
